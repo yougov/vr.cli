9351c483d60fc4c1750c947c7e521cdb7e37ac90
Jason R. Coombs <jaraco@jaraco.com>
2014-07-11 07:39:41 -0400

    Dropping support for Python 2.6

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup_params = dict(
 	classifiers = [
 		"Development Status :: 5 - Production/Stable",
 		"Intended Audience :: System Administrators",
-		"Programming Language :: Python :: 2.6",
 		"Programming Language :: Python :: 2.7",
 		"Programming Language :: Python :: 3",
 	],
