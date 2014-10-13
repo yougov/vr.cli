d9ce8a3e8455d126d1e129e7d0cbd50d373864b0
Jason R. Coombs <jaraco@jaraco.com>
2014-10-13 17:49:44 +0100

    YouGov prefers spaces to tabs

--- setup.py
+++ setup.py
@@ -3,41 +3,41 @@
 import setuptools
 
 with open('README') as stream:
-	long_description = stream.read()
+    long_description = stream.read()
 
 setup_params = dict(
-	name='vr.cli',
-	use_hg_version=dict(increment='0.1'),
-	description="Command-line client for working with Velociraptor",
-	long_description=long_description,
-	author="Jason R. Coombs",
-	author_email="jaraco@jaraco.com",
-	url="https://bitbucket.org/yougov/vr.cli",
-	packages=setuptools.find_packages(),
-	namespace_packages=['vr'],
-	zip_safe=False,
-	classifiers = [
-		"Development Status :: 5 - Production/Stable",
-		"Intended Audience :: System Administrators",
-		"Programming Language :: Python :: 2.7",
-		"Programming Language :: Python :: 3",
-	],
-	entry_points=dict(
-		console_scripts=[
-			'vr.cli = vr.cli:handle_command_line',
-		],
-	),
-	install_requires=[
-		'jaraco.util>=8.5,<11dev',
-		'vr.common>=3.7.1',
-		'more_itertools',
-		'six',
-		'datadiff>=1.1.6',
-		'pyyaml',
-	],
-	setup_requires=[
-		'hgtools',
-	],
+    name='vr.cli',
+    use_hg_version=dict(increment='0.1'),
+    description="Command-line client for working with Velociraptor",
+    long_description=long_description,
+    author="Jason R. Coombs",
+    author_email="jaraco@jaraco.com",
+    url="https://bitbucket.org/yougov/vr.cli",
+    packages=setuptools.find_packages(),
+    namespace_packages=['vr'],
+    zip_safe=False,
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points=dict(
+        console_scripts=[
+            'vr.cli = vr.cli:handle_command_line',
+        ],
+    ),
+    install_requires=[
+        'jaraco.util>=8.5,<11dev',
+        'vr.common>=3.7.1',
+        'more_itertools',
+        'six',
+        'datadiff>=1.1.6',
+        'pyyaml',
+    ],
+    setup_requires=[
+        'hgtools',
+    ],
 )
 if __name__ == '__main__':
-	setuptools.setup(**setup_params)
+    setuptools.setup(**setup_params)
--- vr/cli.py
+++ vr/cli.py
@@ -18,202 +18,202 @@ from vr.common import models
 
 
 class FilterExcludeAction(argparse.Action):
-	def __call__(self, parser, namespace, values, option_string=None):
-		namespace.filter.exclusions.append(values)
+    def __call__(self, parser, namespace, values, option_string=None):
+        namespace.filter.exclusions.append(values)
 
 
 class Swarm(cmdline.Command):
-	@classmethod
-	def add_arguments(cls, parser):
-		parser.add_argument('filter', type=models.SwarmFilter)
-		parser.add_argument('tag')
-		parser.add_argument('-x', '--exclude', action=FilterExcludeAction)
-		parser.add_argument(
-			'--countdown', action="store_true",
-			default=False,
-			help="Give a 5 second countdown before dispatching swarms.",
-		)
+    @classmethod
+    def add_arguments(cls, parser):
+        parser.add_argument('filter', type=models.SwarmFilter)
+        parser.add_argument('tag')
+        parser.add_argument('-x', '--exclude', action=FilterExcludeAction)
+        parser.add_argument(
+            '--countdown', action="store_true",
+            default=False,
+            help="Give a 5 second countdown before dispatching swarms.",
+        )
 
-	@classmethod
-	def run(cls, args):
-		swarms = models.Swarm.load_all(args.vr)
-		matched = list(args.filter.matches(swarms))
-		print("Matched", len(matched), "apps")
-		pprint.pprint(matched)
-		if args.countdown:
-			ui.countdown("Reswarming in {} sec")
-		changes = dict()
-		if args.tag != '-':
-			changes['version'] = args.tag
-		[swarm.dispatch(**changes) for swarm in matched]
+    @classmethod
+    def run(cls, args):
+        swarms = models.Swarm.load_all(args.vr)
+        matched = list(args.filter.matches(swarms))
+        print("Matched", len(matched), "apps")
+        pprint.pprint(matched)
+        if args.countdown:
+            ui.countdown("Reswarming in {} sec")
+        changes = dict()
+        if args.tag != '-':
+            changes['version'] = args.tag
+        [swarm.dispatch(**changes) for swarm in matched]
 
 
 class Build(cmdline.Command):
-	@classmethod
-	def add_arguments(cls, parser):
-		parser.add_argument('app')
-		parser.add_argument('tag')
+    @classmethod
+    def add_arguments(cls, parser):
+        parser.add_argument('app')
+        parser.add_argument('tag')
 
-	@classmethod
-	def run(cls, args):
-		build = models.Build._for_app_and_tag(args.vr, args.app, args.tag)
-		build.assemble()
+    @classmethod
+    def run(cls, args):
+        build = models.Build._for_app_and_tag(args.vr, args.app, args.tag)
+        build.assemble()
 
 
 class RebuildAll(cmdline.Command):
-	@classmethod
-	def add_arguments(cls, parser):
-		parser.add_argument('filter', type=models.SwarmFilter)
-		parser.add_argument('-x', '--exclude', action=FilterExcludeAction)
-		parser.add_argument(
-			'--countdown', action="store_true",
-			default=False,
-			help="Give a 5 second countdown before dispatching swarms.",
-		)
+    @classmethod
+    def add_arguments(cls, parser):
+        parser.add_argument('filter', type=models.SwarmFilter)
+        parser.add_argument('-x', '--exclude', action=FilterExcludeAction)
+        parser.add_argument(
+            '--countdown', action="store_true",
+            default=False,
+            help="Give a 5 second countdown before dispatching swarms.",
+        )
 
-	@classmethod
-	def run(cls, args):
-		swarms = models.Swarm.load_all(args.vr)
-		swarms = list(args.filter.matches(swarms))
-		print("Matched", len(swarms), "apps")
-		pprint.pprint(swarms)
-		if args.countdown:
-			models.countdown("Rebuilding in {} sec")
-		builds = [swarm.new_build() for swarm in swarms]
-		for build in set(builds):
-			build.assemble()
-		for swarm in swarms:
-			swarm.dispatch()
+    @classmethod
+    def run(cls, args):
+        swarms = models.Swarm.load_all(args.vr)
+        swarms = list(args.filter.matches(swarms))
+        print("Matched", len(swarms), "apps")
+        pprint.pprint(swarms)
+        if args.countdown:
+            models.countdown("Rebuilding in {} sec")
+        builds = [swarm.new_build() for swarm in swarms]
+        for build in set(builds):
+            build.assemble()
+        for swarm in swarms:
+            swarm.dispatch()
 
 
 class FilterParam(object):
 
-	@classmethod
-	def add_arguments(cls, parser):
-		parser.add_argument('filter', type=models.SwarmFilter, nargs='?',
-			default=models.SwarmFilter())
+    @classmethod
+    def add_arguments(cls, parser):
+        parser.add_argument('filter', type=models.SwarmFilter, nargs='?',
+            default=models.SwarmFilter())
 
 
 class Procs(FilterParam, cmdline.Command):
 
-	swarmtmpl = '{swarm.name} [{swarm.version}]'
-	proctmpl = '{host:<22}  {port:<5}  {statename:<9}  {description}'
+    swarmtmpl = '{swarm.name} [{swarm.version}]'
+    proctmpl = '{host:<22}  {port:<5}  {statename:<9}  {description}'
 
-	@classmethod
-	def add_arguments(cls, parser):
-		action_lookup = dict(
-			list=cls._list,
-			stop=partial(cls._exec, 'stop'),
-			start=partial(cls._exec, 'start'),
-			restart=partial(cls._exec, 'restart'),
-		)
-		parser.add_argument('subcmd', type=lambda val: action_lookup[val])
-		super(Procs, cls).add_arguments(parser)
+    @classmethod
+    def add_arguments(cls, parser):
+        action_lookup = dict(
+            list=cls._list,
+            stop=partial(cls._exec, 'stop'),
+            start=partial(cls._exec, 'start'),
+            restart=partial(cls._exec, 'restart'),
+        )
+        parser.add_argument('subcmd', type=lambda val: action_lookup[val])
+        super(Procs, cls).add_arguments(parser)
 
-	@classmethod
-	def run(cls, args):
-		all_swarms = models.Swarm.load_all(args.vr)
-		swarms = args.filter.matches(all_swarms)
-		consume(map(args.subcmd, swarms))
+    @classmethod
+    def run(cls, args):
+        all_swarms = models.Swarm.load_all(args.vr)
+        swarms = args.filter.matches(all_swarms)
+        consume(map(args.subcmd, swarms))
 
-	@staticmethod
-	def _get_proc_from_dict(proc):
-		host = models.Host(proc['host'])
-		return host.get_proc(proc['group'])
+    @staticmethod
+    def _get_proc_from_dict(proc):
+        host = models.Host(proc['host'])
+        return host.get_proc(proc['group'])
 
-	@classmethod
-	def _list(cls, swarm):
-		print()
-		print(cls.swarmtmpl.format(**vars()))
-		for proc in swarm.procs:
-			print('  ' + cls.proctmpl.format(**proc))
+    @classmethod
+    def _list(cls, swarm):
+        print()
+        print(cls.swarmtmpl.format(**vars()))
+        for proc in swarm.procs:
+            print('  ' + cls.proctmpl.format(**proc))
 
-	@classmethod
-	def _exec(cls, proc_method, swarm):
-		print()
-		print(cls.swarmtmpl.format(**vars()))
-		for proc in swarm.procs:
-			print(proc_method.upper() + ' ' + cls.proctmpl.format(**proc))
-			getattr(cls._get_proc_from_dict(proc), proc_method)()
+    @classmethod
+    def _exec(cls, proc_method, swarm):
+        print()
+        print(cls.swarmtmpl.format(**vars()))
+        for proc in swarm.procs:
+            print(proc_method.upper() + ' ' + cls.proctmpl.format(**proc))
+            getattr(cls._get_proc_from_dict(proc), proc_method)()
 
 
 class ListSwarms(FilterParam, cmdline.Command):
 
-	@classmethod
-	def run(cls, args):
-		with timing.Stopwatch() as watch:
-			all_swarms = models.Swarm.load_all(args.vr)
-		tmpl = "Loaded {n_swarms} swarms in {watch.elapsed}"
-		msg = tmpl.format(n_swarms=len(all_swarms), watch=watch)
-		print(msg)
-		filtered_swarms = args.filter.matches(all_swarms)
-		consume(map(print, sorted(filtered_swarms)))
+    @classmethod
+    def run(cls, args):
+        with timing.Stopwatch() as watch:
+            all_swarms = models.Swarm.load_all(args.vr)
+        tmpl = "Loaded {n_swarms} swarms in {watch.elapsed}"
+        msg = tmpl.format(n_swarms=len(all_swarms), watch=watch)
+        print(msg)
+        filtered_swarms = args.filter.matches(all_swarms)
+        consume(map(print, sorted(filtered_swarms)))
 
 
 class Uptests(cmdline.Command):
 
-	@classmethod
-	def run(cls, args):
-		resp = args.vr.load('/api/v1/testruns/latest/')
-		results = resp['testresults']
-		for result in results:
-			if not result['passed']:
-				print("{procname} failed:".format(**result))
-				print(result['results'])
+    @classmethod
+    def run(cls, args):
+        resp = args.vr.load('/api/v1/testruns/latest/')
+        results = resp['testresults']
+        for result in results:
+            if not result['passed']:
+                print("{procname} failed:".format(**result))
+                print(result['results'])
 
 
 class Deploy(cmdline.Command):
 
-	@staticmethod
-	def find_release(spec):
-		if not spec.isdigit():
-			raise NotImplementedError("Release spec must be a release ID")
-		return int(spec)
+    @staticmethod
+    def find_release(spec):
+        if not spec.isdigit():
+            raise NotImplementedError("Release spec must be a release ID")
+        return int(spec)
 
-	@classmethod
-	def add_arguments(cls, parser):
-		parser.add_argument('release', type=cls.find_release)
-		parser.add_argument('host')
-		parser.add_argument('port', type=int)
-		parser.add_argument('proc')
-		parser.add_argument('-c', '--config-name', default='prod')
+    @classmethod
+    def add_arguments(cls, parser):
+        parser.add_argument('release', type=cls.find_release)
+        parser.add_argument('host')
+        parser.add_argument('port', type=int)
+        parser.add_argument('proc')
+        parser.add_argument('-c', '--config-name', default='prod')
 
-	@classmethod
-	def run(cls, args):
-		release = models.Release(args.vr)
-		release.load(models.Release.base + str(args.release) + '/')
-		release.deploy(args.host, args.port, args.proc, args.config_name)
+    @classmethod
+    def run(cls, args):
+        release = models.Release(args.vr)
+        release.load(models.Release.base + str(args.release) + '/')
+        release.deploy(args.host, args.port, args.proc, args.config_name)
 
 
 class CompareReleases(cmdline.Command):
-	@classmethod
-	def add_arguments(cls, parser):
-		parser.add_argument('orig', type=Deploy.find_release)
-		parser.add_argument('changed', type=Deploy.find_release)
+    @classmethod
+    def add_arguments(cls, parser):
+        parser.add_argument('orig', type=Deploy.find_release)
+        parser.add_argument('changed', type=Deploy.find_release)
 
-	@classmethod
-	def load_config(cls, args, release_id):
-		release = models.Release(args.vr)
-		template = models.Release.base + '{release_id}/'
-		release.load(template.format(**locals()))
-		config = io.StringIO(release.config_yaml)
-		return yaml.safe_load(config)
+    @classmethod
+    def load_config(cls, args, release_id):
+        release = models.Release(args.vr)
+        template = models.Release.base + '{release_id}/'
+        release.load(template.format(**locals()))
+        config = io.StringIO(release.config_yaml)
+        return yaml.safe_load(config)
 
-	@classmethod
-	def run(cls, args):
-		orig = cls.load_config(args, args.orig)
-		changed = cls.load_config(args, args.changed)
-		print(datadiff.diff(orig, changed))
+    @classmethod
+    def run(cls, args):
+        orig = cls.load_config(args, args.orig)
+        changed = cls.load_config(args, args.changed)
+        print(datadiff.diff(orig, changed))
 
 
 def handle_command_line():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--url',
-		help="Velociraptor URL (defaults to https://deploy, resolved; "
-			"override with VELOCIRAPTOR_URL)")
-	parser.add_argument('--username',
-		help="Override the username used for authentication")
-	cmdline.Command.add_subparsers(parser)
-	args = parser.parse_args()
-	args.vr = models.Velociraptor(args.url, args.username)
-	args.action.run(args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url',
+        help="Velociraptor URL (defaults to https://deploy, resolved; "
+            "override with VELOCIRAPTOR_URL)")
+    parser.add_argument('--username',
+        help="Override the username used for authentication")
+    cmdline.Command.add_subparsers(parser)
+    args = parser.parse_args()
+    args.vr = models.Velociraptor(args.url, args.username)
+    args.action.run(args)
